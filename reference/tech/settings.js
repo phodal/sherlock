0b92f7b20497e6ee1f1f39e08ed2c9e1954a52af
Fengda HUANG <h@phodal.com>
2015-02-28 13:24:37 +0800

    Add civ 5 tech web as reference

---
+++ reference/tech/settings.js
@@ -0,0 +1,19 @@
+// type of positioning ... static, fixed, relative, absolute ...
+var techWebPositioning = "absolute";
+
+// set top y coord for canvas
+var techWebTop = "0";
+
+// set left x coord for canvas
+var techWebLeft = "0";
+
+// set width of canvas in pixels
+//var w = 600;
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+// set height of canvas in pixels
+//var h = 400;
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+// relative or absolute path to all the image assets
+var assetsPath = "img/";
