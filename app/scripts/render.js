441fde9b9e2d7e3cc780a7fcc70fd505453e3b27
Fengda HUANG <h@phodal.com>
2015-03-04 23:14:10 +0800

    move some step to function

--- app/scripts/render.js
+++ app/scripts/render.js
@@ -2,27 +2,32 @@ define(['d3', 'lib/knockout', 'scripts/Utils', 'dagre-d3', 'jquery','lettuce', '
   function (d3, ko, Utils, dagreD3, $, Lettuce) {
     'use strict';
     function renderPage(skills_data) {
+      function setSkillNode() {
+        ko.utils.arrayForEach(skills_data.skills, function (skill) {
+          var value = skill;
+          value.label = skill.name;
+          value.rx = value.ry = 5;
+          g.setNode(skill.name, value);
+        });
+      }
+
+      function setSkillEdge() {
+        ko.utils.arrayForEach(skills_data.skills, function (skill) {
+          var skill_id = skill.id;
+          if (skill.depends) {
+            ko.utils.arrayForEach(skill.depends, function (id) {
+              var dependents_name = Utils.getSkillById(skills_data.skills, id).name;
+              var skill_name = Utils.getSkillById(skills_data.skills, skill_id).name;
+              g.setEdge(dependents_name, skill_name, {label: '', lineInterpolate: 'basis'});
+            });
+          }
+        });
+      }
+
       var lettuce = new Lettuce();
       var g = new dagreD3.graphlib.Graph().setGraph({});
-
-      ko.utils.arrayForEach(skills_data.skills, function (skill) {
-        var value = skill;
-        value.label = skill.name;
-        value.rx = value.ry = 5;
-        g.setNode(skill.name, value);
-      });
-
-      ko.utils.arrayForEach(skills_data.skills, function (skill) {
-        var skill_id = skill.id;
-        if (skill.depends) {
-          ko.utils.arrayForEach(skill.depends, function (id) {
-	          var dependents_name = Utils.getSkillById(skills_data.skills, id).name;
-	          var skill_name = Utils.getSkillById(skills_data.skills, skill_id).name;
-            g.setEdge(dependents_name, skill_name, {label: '',lineInterpolate: 'basis'});
-          });
-        }
-      });
-
+      setSkillNode();
+      setSkillEdge();
       var render = new dagreD3.render();
 
       var svg = d3.select('svg'),
@@ -37,8 +42,9 @@ define(['d3', 'lib/knockout', 'scripts/Utils', 'dagre-d3', 'jquery','lettuce', '
             description: g.node(v).description
           };
 		      var results = lettuce.Template.tmpl('<p class="name">{%=o.name%}</p><p class="description">{%=o.description%}</p>', data);
-            return results;
+          return results;
         })
+        .on("click", console.log("========"))
         .each(function (v) {
           $(this).tipsy({gravity: 's', opacity: 1, html: true});
         });
